cb0c2e0ede10a825acffc7acffc835e84872f1de
renanpchaves <nanerpc@gmail.com>
2026-03-03 22:04:38 -0300

    💿add rating

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from modelo.darkmoon import Albums
 
 cd1 = Albums('The Dark Side of the Moon', 'Pink Floyd', 10)
+cd1.receber_avaliacao('Rn', 10)
+cd1.receber_avaliacao('Jun', 8)
+cd1.receber_avaliacao('Mik', 2)
 cd1.estoque_alt()
 
 cd2 = Albums('Abbey Road', 'The Beatles', 17)
--- modelo/darkmoon.py
+++ modelo/darkmoon.py
@@ -1,3 +1,5 @@
+from modelo.rating import Rating
+
 class Albums:
     album = []
 
@@ -6,6 +8,7 @@ class Albums:
         self._artista = artista.upper()
         self._quant_musicas = quant_musicas
         self._ativo = False
+        self._avaliacao = []
         Albums.album.append(self)
 
     def __str__(self):
@@ -13,9 +16,9 @@ class Albums:
 
     @classmethod
     def listar_albums(cls):
-        print (f'{'Nome do Album'.ljust(28)} | {'Artista'.ljust(25)} | {'Número de faixas'.ljust(25)} | Estoque')
+        print (f'{'Nome do Album'.ljust(28)} | {'Artista'.ljust(25)} | {'Número de faixas'.ljust(25)} | {'Avaliação'.ljust(25)} | Estoque')
         for i in Albums.album:
-            print (f'{i._nome.ljust(28)} | {i._artista.ljust(25)} | {i._quant_musicas:<25} | {i.ativo}')
+            print (f'{i._nome.ljust(28)} | {i._artista.ljust(25)} | {str(i._quant_musicas).ljust(25)} | {str(i.media_avaliacoes).ljust(25)} | {i.ativo}')
 
     @property
     def ativo(self):
@@ -23,3 +26,16 @@ class Albums:
 
     def estoque_alt(self):
         self._ativo = not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Rating(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_notas / quantidade_notas, 1)
+        return f'⭐ {media}'
